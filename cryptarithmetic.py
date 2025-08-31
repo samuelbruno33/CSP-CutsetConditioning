8466b12a6ef155c68c0fc9950651fc761962a819
Samuel Bruno <52835494+samuelbruno33@users.noreply.github.com>
2025-08-31 23:45:12 +0200

    Update cryptarithmetic.py

--- cryptarithmetic.py
+++ cryptarithmetic.py
@@ -32,10 +32,10 @@ def send_more_money_csp():
 def t_plus_t_eq_ee_csp():
     """
     T + T = EE
-    2 variabili, dominio 0-9 con T diverso da 0
+    2 variabili, dominio 0-9 con T,E diverso da 0
     """
     variables = ['T','E']
-    domains   = {'T': list(range(1,10)), 'E': list(range(10))}
+    domains   = {'T': list(range(1,10)), 'E': list(range(1,10))}
     constraints = [
         (('T','E'), lambda T,E: 2*T == 11*E)
     ]
